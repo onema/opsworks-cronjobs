7299250c20b93dcb34a83d59f1f18591bd2b6c93
Juan Manuel Torres <kinojman@gmail.com>
2015-06-12 22:01:13 -0700

    First commit.

---
+++ cronjobs/metadata.rb
@@ -0,0 +1,7 @@
+name              "cronjobs"
+maintainer        "Juan Manuel Torres"
+maintainer_email  "kinojman@gmail.com"
+license           "Apache 2"
+description       "Sets or updates using the cron resource and the custom JSON."
+version           "1.0.0"
+
