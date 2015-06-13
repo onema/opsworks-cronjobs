322aff66f039d08b3c7b314957e686679079acfc
Juan Manuel Torres <kinojman@gmail.com>
2015-06-13 11:18:55 -0700

    Update metadata license

--- cronjobs/metadata.rb
+++ cronjobs/metadata.rb
@@ -1,7 +1,7 @@
 name              "cronjobs"
 maintainer        "Juan Manuel Torres"
 maintainer_email  "kinojman@gmail.com"
-license           "Apache 2"
+license           "Apache License v2"
 description       "Sets or updates using the cron resource and the custom JSON."
 version           "1.0.0"
 
